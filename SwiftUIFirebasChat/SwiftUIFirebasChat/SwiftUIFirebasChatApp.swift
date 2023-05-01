062c46de0ffb548e247515a55b0b7f0bee0557a3
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-05-01 19:14:05 +0300

    create Xcode project

---
+++ SwiftUIFirebasChat/SwiftUIFirebasChat/SwiftUIFirebasChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebasChatApp.swift
+//  SwiftUIFirebasChat
+//
+//  Created by Ahmed Amin on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebasChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
